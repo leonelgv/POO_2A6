4d681650acff4963e1aba609fd5090e863223bee
Leonel Gonzalez Vidales <leonelgv@gmail.com>
2019-03-09 18:22:42 -0600

    Feat: Estructura swith agregada

--- EstructurasControl.py
+++ EstructurasControl.py
@@ -51,7 +51,25 @@ class EstructurasControl:
                 break
         return resultado
 
+    def switch_demo(self, argument):
+        switcher = {
+            1: "January",
+            2: "February",
+            3: "March",
+            4: "April",
+            5: "May",
+            6: "June",
+            7: "July",
+            8: "August",
+            9: "September",
+            10: "October",
+            11: "November",
+            12: "December"
+        }
+        print (switcher.get(argument, "Invalid month"))
 
+EC = EstructurasControl()
+EC.switch_demo(4)
 
 if __name__==  '__main__':
     import doctest
